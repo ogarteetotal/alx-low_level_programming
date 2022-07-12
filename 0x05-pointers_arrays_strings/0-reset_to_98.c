355228a733f8450d8cefcda30882378e61fb6782
ogarteetot <ogarteetotal@yahoo.com>
2022-07-12 10:04:17 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,6 @@
 
 /**
  * reset_to_98 - Takes a pointer to an int and updates
- *
  * the value it points to to 98.
  *
  * n: The pointer to an int.
@@ -12,6 +11,6 @@ void reset_to_98(int *n)
 
 {
 
-	*n = 98;
+ *n = 98;
 
 }
