81271fb0c44c231bcbead5c967313e57df4a217a
ogarteetot <ogarteetotal@yahoo.com>
2022-07-12 09:39:07 -0700

    commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 
-
-
 /**
  *
- *  * reset_to_98 - Takes a pointer to an int and updates
+ * reset_to_98 - Takes a pointer to an int and updates
  *
- *   *               the value it points to to 98.
+ * the value it points to to 98.
  *
- *    * @n: The pointer to an int.
+ * n: The pointer to an int.
  *
- *     */
+ */
 
 void reset_to_98(int *n)
 
