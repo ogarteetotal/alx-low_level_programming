38949d37069195342fec7ae6935a6279b3883c72
ogarteetot <ogarteetotal@yahoo.com>
2022-07-14 06:47:43 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * _puts - Prints a string to stdout.
+ * @str: The string to be printed.
+ */
+
+void _puts(char *str)
+
+{
+	while (*str)
+	_putchar(*str++);
+	_putchar('\n');
+
+}
