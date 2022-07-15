33248e4aa4174a91cdadce31a625db781efcf301
ogarteetot <ogarteetotal@yahoo.com>
2022-07-15 07:54:12 -0700

    header file main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 
+int main(void)                                                                                                                                                
 void dummy (void);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
