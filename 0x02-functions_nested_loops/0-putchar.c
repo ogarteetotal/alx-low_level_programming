f79f8af0645ea517e62807777c2a81dc176eb824
ogarteetot <ogarteetotal@yahoo.com>
2022-07-16 08:57:12 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * main - Check description
+ * Description: Write a program that prints _putchar, followed by a new line.
+ * Return: 0.
+ */
+
+int main(void)
+
+{
+
+	char word[8] = "_putchar";
+	int i;
+
+	for (i = 0; i < 9; i++)
+	_putchar(word[i]);
+	_putchar('\n');
+	return (0);
+}
