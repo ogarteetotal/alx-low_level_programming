08e6451ac05510a7e94656d8094f614fb7cecf1e
ogarteetot <ogarteetotal@yahoo.com>
2022-07-16 06:20:16 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -13,17 +13,16 @@ int main(void)
 {
 
 	char low, e, q;
+
 	e = 'e';
 	q = 'q';
 
 	for (low = 'a'; low <= 'z'; low++)
 	{
-	
 	if (low != e && low != q)
 	putchar(low);
 	}
 	putchar('\n');
-
 	return (0);
 
 }
