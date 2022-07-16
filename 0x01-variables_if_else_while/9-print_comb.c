5a8b9d6228db484baa6626afc9f8266be12bcaa8
ogarteetot <ogarteetotal@yahoo.com>
2022-07-16 06:52:04 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,7 +12,6 @@
 int main(void)
 
 {
-
 	int d;
 
 	for (d = '0'; d <= '9'; d++)
@@ -20,13 +19,11 @@ int main(void)
 	putchar(d);
 	if (d != '9')
 	{	
-	
 	putchar(',');
 	putchar(' ');
-
 	}
 	}
 	putchar('\n');
-	return (0);
 
+	return (0);
 }
