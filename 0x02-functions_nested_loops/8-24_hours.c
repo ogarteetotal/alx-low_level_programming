bf891f632fc60c4cadc7c8ba4278d5a01be02697
ogarteetot <ogarteetotal@yahoo.com>
2022-07-16 12:32:18 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -20,12 +20,12 @@ void jack_bauer(void)
 	{
  	for (d = 0; d <= 9; d++)
 	{
-	_putchar(a + '0');
-	_putchar(b + '0');
-	_putchar(58);
-	_putchar(c + '0');
-	_putchar(d + '0');
-	_putchar('\n');
+		_putchar(a + '0');
+		_putchar(b + '0');
+		_putchar(58);
+		_putchar(c + '0');
+		_putchar(d + '0');
+		_putchar('\n');
 	}
 	}
 	}
