94cfa4cdd2b82e662b2941dd16321f472acd10bf
ogarteetot <ogarteetotal@yahoo.com>
2022-07-16 12:28:44 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,20 +8,16 @@
 void jack_bauer(void)
 
 {
-
 	int a, b, c, d;
 
 	for (a = 0; a <= 2; a++)
 	{
-
 	for (b = 0; b <= 9; b++)
 	{
 	if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
 	{
-
 	for (c = 0; c <= 5; c++)
 	{
-
  	for (d = 0; d <= 9; d++)
 	{
 	_putchar(a + '0');
