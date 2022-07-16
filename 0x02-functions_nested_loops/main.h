a423aea2acc14e570f67bbe3aa63d53bcef3c999
ogarteetot <ogarteetotal@yahoo.com>
2022-07-16 10:44:03 -0700

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,3 @@
-#ifndef MAIN_H                                                                  
-#define MAIN_H                
-
-#include <stdio.h> 
-
 int _putchar(char c);
 
 void print_alphabet(void);
@@ -21,11 +16,10 @@ int print_last_digit(int);
 
 void jack_bauer(void);
 
-void times_table(void)
+void times_table(void);
 
 int add(int, int);
 
 void print_to_98(int n);
 
 void print_times_table(int n);
-#endif  
