475565b963550dcee87b7bc7314501bd8fa0bec0
ogarteetot <ogarteetotal@yahoo.com>
2022-07-16 12:40:36 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,6 +8,7 @@
 void jack_bauer(void)
 
 {
+
 	int a, b, c, d;
 
 	for (a = 0; a <= 2; a++)
@@ -18,14 +19,14 @@ void jack_bauer(void)
 	{
 	for (c = 0; c <= 5; c++)
 	{
- 	for (d = 0; d <= 9; d++)
+	for (d = 0; d <= 9; d++)
 	{
-		_putchar(a + '0');
-		_putchar(b + '0');
-		_putchar(58);
-		_putchar(c + '0');
-		_putchar(d + '0');
-		_putchar('\n');
+	_putchar(a + '0');
+	_putchar(b + '0');
+	_putchar(58);
+	_putchar(c + '0');
+	_putchar(d + '0');
+	_putchar('\n');
 	}
 	}
 	}
