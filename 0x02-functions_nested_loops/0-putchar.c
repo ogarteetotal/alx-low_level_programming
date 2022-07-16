a043cde569f232001ed4ea049150948082899df6
ogarteetot <ogarteetotal@yahoo.com>
2022-07-16 10:48:18 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,13 +9,14 @@ int main(void)
 
 {
 
-	_putchar('W');
-	_putchar('e');
-	_putchar('l');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
 	_putchar('c');
-	_putchar('o');
-	_putchar('m');
-	_putchar('e');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 
