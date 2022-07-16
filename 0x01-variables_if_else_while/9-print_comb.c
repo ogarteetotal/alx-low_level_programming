9fedddf64a2fd3ceabef3710bc1b2f8370e1a4e3
ogarteetot <ogarteetotal@yahoo.com>
2022-07-16 07:06:51 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,18 +2,15 @@
 #include <stdlib.h>
 #include <time.h>
 
-
 /**
  * main - Entry point
  * Return: Always 0 (Success)
  */
 
-
 int main(void)
 
 {
 	int d;
-
 	for (d = '0'; d <= '9'; d++)
 	{
 	putchar(d);
