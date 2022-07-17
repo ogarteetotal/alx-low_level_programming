8d23ecf97fec912a7368ba652327749fce0bc12e
ogarteetot <ogarteetotal@yahoo.com>
2022-07-16 19:31:35 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,7 +4,6 @@
  * print_times_table - Prints a multiplication table up to param
  * @n: The number to be treated
  * Return: Number matrix
- *
  */
 
 void print_times_table(int n)
@@ -12,8 +11,7 @@ void print_times_table(int n)
 {
 
 	int x, y, z;
-	
-	if (n >= 0 && n <= 14)
+											if (n >= 0 && n <= 14)
 	{
 	for (x = 0; x <= n; x++)
 	{
