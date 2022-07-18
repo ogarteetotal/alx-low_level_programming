361d72a561cd7c9f50e253153e303d0480a6ed2e
ogarteetot <ogarteetotal@yahoo.com>
2022-07-18 07:49:36 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -14,14 +14,13 @@
 	unsigned int ni = 0;
 	int min = 1;
 	int isi = 0;
-
 	while (s[c])
 	{
 	if (s[c] == 45)
 	{
 	min *= -1;
 	}
-											while (s[c] >= 48 && s[c] <= 57)
+	while (s[c] >= 48 && s[c] <= 57)
 	{
 	isi = 1;
 	ni = (ni * 10) + (s[c] - '0');
@@ -36,5 +35,3 @@
 	ni *= min;
 	return (ni);
 }
-
-
