990f44a0d4ce55a6e3913229d63774d465c2018c
root <ogarteetotal@yahoo.com>
2022-12-08 14:39:28 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
 
 /**
  * main - prints "Programming is like building a multilingual
- * puzzle, followed by a newline."
- * return 0.
+ * puzzle, followed by a newline.
+ * Return: Always 0.
  */
 
 
@@ -13,6 +13,6 @@ int main(void)
 
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 
 }
