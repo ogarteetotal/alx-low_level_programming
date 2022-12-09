65bf94fe30454f8cef641c123f305c2851dca867
root <ogarteetotal@yahoo.com>
2022-12-08 18:59:57 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,7 @@
 #include <unistd.h>
 
-/**main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+/**
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
  * followed by a newline, to standard error.
  * Return: Always 1.
  */
