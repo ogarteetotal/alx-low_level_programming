d7cdd87ff58560a8ceb2c30d635a8cc67688df0a
root <ogarteetotal@yahoo.com>
2022-12-19 13:54:31 -0800

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,14 +9,11 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int i;
-
-	for (i = 0; src[i] != '\0'; i++)
-	{	
-		dest[i] = src[i];
-	}
-
-	dest[i++] = '\0';
-
-	return (dest);
+int i;
+for (i = 0; src[i] != '\0'; i++)
+{
+dest[i] = src[i];
+}
+dest[i++] = '\0';
+return (dest);
 }
