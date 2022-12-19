65f53ef7053755daabb0e970c51413a41c53db48
root <ogarteetotal@yahoo.com>
2022-12-19 12:21:51 -0800

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+
+/**
+ * rev_string - Reverses a string
+ * @s: The string to be modified
+ * Return: void
+ */
+
+void rev_string(char *s)
+
+{
+
+	int len = 0, index = 0;
+	char tmp;
+
+	while (s[index++])
+	len++;
+
+	for (index = len - 1; index >= len / 2; index--)
+	{
+	tmp = s[index];
+	s[index] = s[len - index - 1];
+	s[len - index - 1] = tmp;
+	}
+}
