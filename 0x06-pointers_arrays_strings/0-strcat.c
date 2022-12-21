55dd61362b71e89e319c030e783eb32babcd78f7
root <ogarteetotal@yahoo.com>
2022-12-21 11:42:12 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,22 +7,24 @@
  * Append src to the end of dest
  */
 
-char *strcat(char *dest, char *src)
+char *strcat(char *dest, const char *src)
 
 {
- char *dest_end = dest;
+  char *dest_end = dest;
 
- while (*dest_end != '\0') 
- {
-  dest_end++;
- }
- while (*src != '\0') 
- {
-  *dest_end = *src;
-  dest_end++;
-  src++;
- }
 
- *dest_end = '\0';
-return (dest);
+  while (*dest_end != '\0')
+  {
+    dest_end++;
+  }
+  while (*src != '\0')
+  {
+    *dest_end = *src;
+    dest_end++;
+    src++;
+  }
+
+  *dest_end = '\0';
+  return (dest);
+
 }
