4d58131529a65652dc4bb8bfa92cc66784bd72c3
root <ogarteetotal@yahoo.com>
2022-12-21 11:21:27 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,21 +10,19 @@
 char *_strcat(char *dest, char *src)
 
 {
+ char *dest_end = dest;
 
-    	  char *dest_end = dest;
-	 
-	  while (*dest_end != '\0') 
-	      {
-		  dest_end++;
-	      }
-	 
-     	  while (*src != '\0') 
-	      {
-	          *dest_end = *src;
-		  dest_end++;
-		  src++;
-	      }
-	      
-	  *dest_end = '\0';
-	  return (dest);
+ while (*dest_end != '\0') 
+ {
+  dest_end++;
+ }
+ while (*src != '\0') 
+ {
+  *dest_end = *src;
+  dest_end++;
+  src++;
+ }
+
+ *dest_end = '\0';
+return (dest);
 }
