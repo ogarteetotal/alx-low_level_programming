4ed50e2a8bfc44280d512262a26618e3a72e0730
root <ogarteetotal@yahoo.com>
2022-12-23 13:45:17 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,37 +1,33 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * cap_string - Capitalizes all words of a string.
- * @str: The string to be capitalized.
- * Return: A pointer to the changed string.
- */
-
-char *cap_string(char *str)
-
+  * cap_string - ...
+  * @s: ...
+  *
+  * Return: char value
+  */
+char *cap_string(char *s)
 {
-	int index = 0;
+	int a = 0, i;
+	int cspc = 13;
+	char spc[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'};
 
-	while (str[index])
+	while (s[a])
 	{
-	while (!(str[index] >= 'a' && str[index] <= 'z'))
-	index++;
-	if (str[index - 1] == ' ' ||
-        str[index - 1] == '\t' ||
-        str[index - 1] == '\n' ||
-        str[index - 1] == ',' ||
-        str[index - 1] == ';' ||
-        str[index - 1] == '.' ||
-        str[index - 1] == '!' ||
-        str[index - 1] == '?' ||
-        str[index - 1] == '"' ||
-        str[index - 1] == '(' ||
-        str[index - 1] == ')' ||
-        str[index - 1] == '{' ||
-        str[index - 1] == '}' ||
-        index == 0)
- 	str[index] -= 32;
-	index++;
-	}
-	return (str);
+		i = 0;
 
+		while (i < cspc)
+		{
+			if ((a == 0 || s[a - 1] == spc[i]) && (s[a] >= 97 && s[a] <= 122))
+				s[a] -= 32;
+
+			i++;
+		}
+
+		a++;
+	}
+
+	return (s);
 }
+
