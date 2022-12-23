a7ea4a64289a30f0457de7d611b0f9a9df8c0001
root <ogarteetotal@yahoo.com>
2022-12-23 13:50:15 -0800

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,96 +1,33 @@
-include "main.h"
+#include "main.h"
+#include <stdio.h>
 
 /**
- * base10 - power in 10 base
- * @n: an exponent
- * Return: returns 10 to power exponent
- *
- */
-
-int base10(int n)
-	
+  * rot13 - ...
+  * @s: ...
+  *
+  * Return: ...
+  */
+char *rot13(char *s)
 {
-	
-	int base = 10;
-		
-	while (n > 0)
-				
+	int a = 0;
+
+	while (s[a])
 	{
-						
-	base *= 10;
-								
-	n--;
-										
+		while ((s[a] >= 'a' && s[a] <= 'z') || (s[a] >= 'A' && s[a] <= 'Z'))
+		{
+			if ((s[a] > 'm' && s[a] <= 'z') || (s[a] > 'M' && s[a] <= 'Z'))
+			{
+				s[a] -= 13;
+				break;
+			}
+
+			s[a] += 13;
+			break;
+		}
+
+		a++;
 	}
-			
-	return (base);
-				
+
+	return (s);
 }
 
-
-
-/**
- *
- *  * print_number - prints integers enters as parameters using putchar
- *
- *   * @n: integer to print
- *
- *    * Return: void
- *
- *     */
-
-void print_number(int n)
-	
-{
-	
-		int power;
-		
-		
-		
-			power = base10(8);
-			
-			
-			
-				if (n < 0)
-					
-						{
-							
-									_putchar('-');
-									
-											n *= -1;
-											
-												}
-				
-				
-				
-					if (n == 0)
-						
-								_putchar('0');
-					
-					
-					
-						else
-							
-								{
-									
-											while (n / power == 0)
-												
-															power /= 10;
-											
-											
-											
-													while (power >= 1)
-														
-																{
-																	
-																				_putchar((n / power) + '0');
-																				
-																							n %= power;
-																							
-																										power /= 10;
-																										
-																												}
-													
-														}
-						
-}
