58b337f21e1bfddd7794fe6fa0a87ea886b438ce
root <ogarteetotal@yahoo.com>
2023-01-06 16:36:26 -0800

    update deleted files

--- 0x09-static_libraries/1-memcpy.c
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
-char *_memcpy(char *dest, char *src, unsigned int n)
-{
-    return memcpy(dest, src, n);
-}
